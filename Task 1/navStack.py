0f0865a7fc9a46e80d7e9850e833064a40611d15
richu101 <richu101@gmail.com>
2022-10-17 18:45:01 +0530

    Reach the point 3 without any collition: tuned PID

--- Task 1/navStack.py
+++ Task 1/navStack.py
@@ -8,13 +8,17 @@ range_max = 1
 # --------------------------
 
 kp = .8
-ki = 0.07
+ki = .08
 kd = 0
 
+kpbl = 2.5   
+kibl = .25
+
 sumoferror = 0
+sumoferrorbl = 0
 # --------------------------
-velocity = .5
-turningvelocity = .1
+velocity = .9
+turningvelocity = .3
 
 regions = {
         'bright':   0,
@@ -32,6 +36,7 @@ def PID(presentvalue):
     global sumoferror
     sumoferror += presentvalue 
     x = (kp*presentvalue)+(ki*sumoferror)
+    print("Pos 1 PID : ",x)
     return x
 def laser_callback(msg):
     global regions,range_max
@@ -43,29 +48,38 @@ def laser_callback(msg):
         'bleft':    min(min(msg.ranges[576:720]), range_max) ,
     }
 
+def pidbl(present,kpbl,kibl,s):
+    global sumoferrorbl
+    setpoint = s
+    err = present - setpoint
+    sumoferrorbl += err 
+    x = kpbl*(err)+kibl*(sumoferrorbl)
+    print("PID Back left ",x)
+    return x    
+
 def pos1error(reg):
     e = 5
-    if(reg == [1,0,0,0,0]):e=-4
-    if(reg == [0,1,0,0,0]):e=-2
+    if(reg == [1,0,0,0,0]):e=-4 +1
+    if(reg == [0,1,0,0,0]):e=-2 +1
     if(reg == [0,0,1,0,0]):e=0
-    if(reg == [0,0,0,1,0]):e=2
-    if(reg == [0,0,0,0,1]):e=4
-    if(reg == [1,1,0,0,0]):e=-3
-    if(reg == [0,1,1,0,0]):e=-1
-    if(reg == [0,0,1,1,0]):e=1
-    if(reg == [0,0,0,1,1]):e=3
-    if(reg == [1,1,1,0,0]):e=-2.5
+    if(reg == [0,0,0,1,0]):e=2 -1
+    if(reg == [0,0,0,0,1]):e=4 -1
+    if(reg == [1,1,0,0,0]):e=-3 +1
+    if(reg == [0,1,1,0,0]):e=-1 +.5
+    if(reg == [0,0,1,1,0]):e=1 -.5
+    if(reg == [0,0,0,1,1]):e=3 -1
+    if(reg == [1,1,1,0,0]):e=-2.5 +1
     if(reg == [1,0,1,0,1]):e=-0.01
     if(reg == [0,1,1,1,0]):e=-0.01
-    if(reg == [0,0,1,1,1]):e=2.5
+    if(reg == [0,0,1,1,1]):e=2.5 -1
     if(reg == [1,1,1,1,0]):e=-0.08
     return e
-def pos4pid(present):
-    global sumoferror
-    setpoint = .5
+def pos4pid(present,s):
+    global sumoferror,kibl,kpbl
+    setpoint = s
     err = present - setpoint
     sumoferror += err 
-    x = kp*(err)+ki*(sumoferror)
+    x = kpbl*(err)+kibl*(sumoferror)
     print("Pos 2 PID ",x)
     return x
 
@@ -100,23 +114,31 @@ def control_loop():
             pos += 1       # Keeping a pos value to know the position of the rover
             state = 1
 
-        if(pos == 1):
+        if(pos == 1 ):
             velocity_msg.linear.x = velocity        # At state 1 the rover starts moving forward 
         
         elif(pos == 2 and intlist == [1,1,1,1,1]):
             velocity_msg.linear.x = turningvelocity         # At state 2 the rover slows at the end and wait for the turning     
             sumoferror = 0
-            velocity_msg.angular.z = pos4pid(distanceList[4])
+            velocity_msg.angular.z = pos4pid(distanceList[4],.5)
 
         elif(pos == 2 and distanceList[2]>.4):
-            if(distanceList[4]>.4):
-                velocity_msg.linear.x = turningvelocity         # At state 2 the rover slows at the end and wait for the turning     
-                sumoferror = 0
-                print("Pos 2 00000")
-                velocity_msg.angular.z = pos4pid(distanceList[3])
-                print("Pos 2 00000",velocity_msg.angular.z)
+            if(distanceList[4]>.5):
+                if(intlist == [1,1,1,0,0] ):
+                    velocity_msg.linear.x = velocity
+                    velocity_msg.angular.z = 0
+                    state=0
+                else:
+                    velocity_msg.linear.x = turningvelocity         # At state 2 the rover slows at the end and wait for the turning     
+                    sumoferror = 0
+                    velocity_msg.angular.z = pos4pid(distanceList[3],.6)
+                    print("Pos 2 00000",velocity_msg.angular.z)
             elif(distanceList[4]<.4):
-                velocity_msg.angular.z = -.3
+                velocity_msg.angular.z = -turningvelocity
+
+        # else:
+        #     velocity_msg.linear.x = velocity  
+
         if(intlist != [1,1,1,1,1] and pos == 1):
             # This case the rover have to move forward till all the sensor value reach 1
             errorv = pos1error(intlist)
@@ -127,6 +149,9 @@ def control_loop():
             else:
                 velocity_msg.linear.x =0
             state = 0
+        if(pos ==3):
+            velocity_msg.linear.x = 0
+            velocity_msg.angular.z = 0        
         print("Pos : ",pos)
         pub.publish(velocity_msg)
         print("Controller message pushed at {}".format(rospy.get_time()))
@@ -139,4 +164,12 @@ if __name__ == '__main__':
     try:
         control_loop()
     except rospy.ROSInterruptException:
-        pass    
+        pass 
+
+
+
+
+
+
+
+
